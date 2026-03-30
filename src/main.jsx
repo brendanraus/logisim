a49ea925504239de1d276c56dd8598a261d82bb7
Brendan Rausenberg <b.rausenberg@gmail.com>
2026-03-30 23:06:40 +0100

    Replace App import with Logisim import

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Logisim from './logisim.tsx'
 
 const style = document.createElement('style')
 style.textContent = `
